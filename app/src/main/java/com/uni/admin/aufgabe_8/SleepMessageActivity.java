b876ad6945275d4fda30ddd953a6beee3a7320b7
Milan_K <allgemein@milan-kostic.de>
2018-01-04 18:29:13 +0100

    Added the funcunality to the SleepAcknowledgeButton.

--- app/src/main/java/com/uni/admin/aufgabe_8/SleepMessageActivity.java
+++ app/src/main/java/com/uni/admin/aufgabe_8/SleepMessageActivity.java
@@ -2,6 +2,7 @@ package com.uni.admin.aufgabe_8;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SleepMessageActivity extends Activity {
 
@@ -9,5 +10,9 @@ public class SleepMessageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep_message);
+    }
+
+    public void finishThis(View view){
+        finish();
     }
 }
