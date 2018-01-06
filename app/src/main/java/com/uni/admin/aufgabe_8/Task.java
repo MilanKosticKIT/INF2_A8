7d5a811e393a758a73cb0cea10cf80c6f36032d7
Milan_K <allgemein@milan-kostic.de>
2018-01-06 16:33:09 +0100

    Added Task.java

---
+++ app/src/main/java/com/uni/admin/aufgabe_8/Task.java
@@ -0,0 +1,27 @@
+package com.uni.admin.aufgabe_8;
+
+/**
+ * Created by admin on 06.01.18.
+ */
+
+public class Task {
+    private String title;
+    private String note;
+
+    public String getTilte(){
+        return this.title;
+    }
+
+    public String getNote(){
+        return this.note;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setNote(String note){
+        this.note = note;
+    }
+
+}
